2eb2769cc598805442e1d223d4de57e10ee478a6
carbonomarciano <queziageane@prof.educacao.sp.gov.br>
2024-08-05 10:50:04 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 600);
+  background("pink");
+  
+}
+
+function draw() {
+ 
+  
+  stroke("black");
+  fill ("blue");
+  
+  // console.log (mouseIsPressed);
+  
+  if (mouseIsPressed) {
+  rect (mouseX,mouseY,50,50);
+  }
+  
+}
